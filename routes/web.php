f6855e8ee4c0795ad5a4fd22524159b6126e449c
wdyogesh <wdyogesh@gmail.com>
2018-09-14 18:27:45 +0530

    add route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    $controllerName = 'UserController';
+    Route::get('user', $controllerName.'@showProfile');
+});
